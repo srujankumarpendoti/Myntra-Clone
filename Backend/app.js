c9e62a6f81f473ce18dfb494956bd47bb4b27b76
srujankumarpendoti <srujankumarpendoti@gmail.com>
2024-09-23 16:33:42 +0530

    second commit

--- Backend/app.js
+++ Backend/app.js
@@ -38,4 +38,4 @@ app.post("/items", async (req, res) => {
   res.status(201).json({ message: "Stored new item.", item: newItem });
 });
 
-app.listen(8080);
+app.listen(8080, console.log("Server Started at 8080"));
